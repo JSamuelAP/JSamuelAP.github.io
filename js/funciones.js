1191624bfb9298851f7bcbebca261f15d21f2a8d
jsamuel <sp4619168@gmail.com>
2022-01-20 19:40:33 -0600

    feat: Mostrar proyectos de forma dinamica
    Se creo un arreglo de objectos, cada objeto contiene informacion
    de los proyectos que se mostraran en la apliacion.
    Se eliminaron los proyectos del HTML, en su lugar se creo un
    template que servira como plantilla para crearlos e insertalos con
    javascript.

--- js/funciones.js
+++ js/funciones.js
@@ -2,11 +2,12 @@ function iniciarApp() {
   // Materialize CSS
   M.AutoInit();
 
-	configurarAnimaciones();
+  configurarAnimaciones();
+  imprimirProyectosUI();
 }
 
 function configurarAnimaciones() {
-	// ScrollReveal
+  // ScrollReveal
   window.sr = ScrollReveal();
 
   sr.reveal(".header__content", {
@@ -33,4 +34,46 @@ function configurarAnimaciones() {
 
   // Hero animation
   document.querySelector("#header").classList.add("animate");
+}
+
+function imprimirProyectosUI() {
+  const proyectsSection = document.querySelector("#proyects .row");
+  const proyectsFragment = document.createDocumentFragment();
+
+  proyectos.forEach((proyecto) => {
+    const cardTemplate = document.querySelector("#card");
+    const cloneCard = cardTemplate.content.cloneNode(true);
+    const { name, imgURL, tags, liveURL, codeURL, description } = proyecto;
+
+    // Imagen
+    cloneCard.querySelector("img").setAttribute("src", imgURL);
+
+    // Titulo
+    cloneCard.querySelector(
+      ".card-content .card-title"
+    ).innerHTML = `${name} <i class="material-icons right">more_vert</i>`;
+
+    // Chips
+    tags.forEach((tag) => {
+      cloneCard.querySelector(
+        ".card__chips"
+      ).innerHTML += `<span class="chip">${tag}</span>`;
+    });
+
+    // Botones
+    cloneCard.querySelector(".card__btn--live").setAttribute("href", liveURL);
+    cloneCard.querySelector(".card__btn--code").setAttribute("href", codeURL);
+
+    // Titulo de la descripcion
+    cloneCard.querySelector(
+      ".card-reveal .card-title"
+    ).innerHTML = `${name}  <i class="material-icons right">close</i>`;
+
+    // Descripcion
+    cloneCard.querySelector(".card-description").textContent = description;
+
+    proyectsFragment.appendChild(cloneCard);
+  });
+
+  proyectsSection.appendChild(proyectsFragment);
 }
---
+++ js/proyectos.js
@@ -0,0 +1,53 @@
+const proyectos = [
+  {
+    name: "Portafolio",
+    imgURL: "images/miniatures/miniature-proyect-1.jpg",
+    tags: ["Materialize", "JS"],
+    liveURL: "https://jsamuelap.github.io",
+    codeURL: "https://github.com/JSamuelAP/JSamuelAP.github.io",
+    description:
+      "Mi portafolio de desarrollador para mostrar mis habilidades 📁.",
+  },
+  {
+    name: "CRUD JS",
+    imgURL: "images/miniatures/miniature-proyect-2.jpg",
+    tags: ["JS", "Bootstrap"],
+    liveURL: "https://jsamuelap.github.io/administrador-asignaturas",
+    codeURL: "https://github.com/JSamuelAP/administrador-asignaturas",
+    description: "Aplicación web para administrar materias escolares 📚.",
+  },
+  {
+    name: "Fylo",
+    imgURL: "images/miniatures/miniature-proyect-3.jpg",
+    tags: ["Bootstrap", "Sass"],
+    liveURL: "https://jsamuelap.github.io/fylo-landing-page",
+    codeURL: "https://github.com/JSamuelAP/fylo-landing-page",
+    description:
+      "Landing Page, este proyecto es un reto 🎯 tomado de Frontend Mentor.",
+  },
+  {
+    name: "WhatsApp UI",
+    imgURL: "images/miniatures/miniature-proyect-4.jpg",
+    tags: ["Materialize", "CSS"],
+    liveURL: "https://jsamuelap.github.io/WhatsApp-UI",
+    codeURL: "https://github.com/JSamuelAP/WhatsApp-UI",
+    description: "Clon de la interfaz principal de WhatsApp Android 📱.",
+  },
+  {
+    name: "Goza México",
+    imgURL: "images/miniatures/miniature-proyect-5.jpg",
+    tags: ["Sass", "JS", "Animaciones"],
+    liveURL: "https://jsamuelap.github.io/goza-mexico",
+    codeURL: "https://github.com/JSamuelAP/goza-mexico",
+    description:
+      "Sitio web de galerías de los lugares más impresionantes de México 🇲🇽.",
+  },
+  {
+    name: "Club Astucia",
+    imgURL: "images/miniatures/miniature-proyect-6.jpg",
+    tags: ["Responsive", "Flexbox", "Grid"],
+    liveURL: "https://jsamuelap.github.io/AstuciaWeb",
+    codeURL: "https://github.com/JSamuelAP/AstuciaWeb",
+    description: "Página web sobre el club de fútbol ficticio Astucia ⚽.",
+  },
+];
