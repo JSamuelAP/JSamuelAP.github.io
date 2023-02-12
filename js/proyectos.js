97a457e4fef608270e6f42b2c457ddb015d008a7
JSamuel <sp4619168@gmail.com>
2023-02-12 11:48:45 -0600

    feat: Proyecto mas reciente agregado

--- js/proyectos.js
+++ js/proyectos.js
@@ -1,79 +1,89 @@
 const proyectos = [
-  // {
-  //   name: "Portafolio",
-  //   imgURL: "images/miniatures/miniature-proyect-1.jpg",
-  //   tags: ["Materialize", "JS"],
-  //   liveURL: "https://jsamuelap.github.io",
-  //   codeURL: "https://github.com/JSamuelAP/JSamuelAP.github.io",
-  //   description:
-  //     "Mi portafolio de desarrollador para mostrar mis habilidades 📁.",
-  // },
-  // {
-  //   name: "Goza México",
-  //   imgURL: "images/miniatures/miniature-proyect-5.jpg",
-  //   tags: ["Sass", "JS", "Animaciones"],
-  //   liveURL: "https://jsamuelap.github.io/goza-mexico",
-  //   codeURL: "https://github.com/JSamuelAP/goza-mexico",
-  //   description:
-  //     "Sitio web de galerías de los lugares más impresionantes de México 🇲🇽.",
-  // },
-  // {
-  //   name: "Club Astucia",
-  //   imgURL: "images/miniatures/miniature-proyect-6.jpg",
-  //   tags: ["Responsive", "Flexbox", "Grid"],
-  //   liveURL: "https://jsamuelap.github.io/AstuciaWeb",
-  //   codeURL: "https://github.com/JSamuelAP/AstuciaWeb",
-  //   description: "Página web sobre el club de fútbol ficticio Astucia ⚽.",
-  // },
-  {
-    name: "Catar 2022",
-    imgURL: "images/miniatures/miniature-proyect-6.jpg",
-    tags: ["JS", "Bootstrap"],
-    liveURL: "https://jsamuelap.github.io/sorteo-catar2022",
-    codeURL: "https://github.com/JSamuelAP/sorteo-catar2022",
-    description: "Simulador del sorteo de grupos del mundial de Catar 🇶🇦🏆⚽.",
-  },
-  {
-    name: "Modo oscuro",
-    imgURL: "images/miniatures/miniature-proyect-5.jpg",
-    tags: ["JS", "Materialize"],
-    liveURL: "https://jsamuelap.github.io/dark-mode",
-    codeURL: "https://github.com/JSamuelAP/dark-mode",
-    description:
-      "Aplicación web que permite cambiar entre modos claro 🌞 y oscuro 🌙.",
-  },
-  {
-    name: "API Clima",
-    imgURL: "images/miniatures/miniature-proyect-4.jpg",
-    tags: ["JS", "Fetch API"],
-    liveURL: "https://jsamuelap.github.io/Clima",
-    codeURL: "https://github.com/JSamuelAP/Clima",
-    description:
-      "Consulta el clima ⛅ de una ciudad en tiempo real gracias a una API.",
-  },
-  {
-    name: "CRUD JS",
-    imgURL: "images/miniatures/miniature-proyect-3.jpg",
-    tags: ["JS", "Bootstrap"],
-    liveURL: "https://jsamuelap.github.io/administrador-asignaturas",
-    codeURL: "https://github.com/JSamuelAP/administrador-asignaturas",
-    description: "Aplicación web para administrar materias escolares 📚.",
-  },
-  {
-    name: "Fylo",
-    imgURL: "images/miniatures/miniature-proyect-2.jpg",
-    tags: ["Bootstrap", "Sass"],
-    liveURL: "https://jsamuelap.github.io/fylo-landing-page",
-    codeURL: "https://github.com/JSamuelAP/fylo-landing-page",
-    description:
-      "Landing Page, este proyecto es un reto 🎯 tomado de Frontend Mentor.",
-  },
-  {
-    name: "WhatsApp UI",
-    imgURL: "images/miniatures/miniature-proyect-1.jpg",
-    tags: ["Materialize", "CSS"],
-    liveURL: "https://jsamuelap.github.io/WhatsApp-UI",
-    codeURL: "https://github.com/JSamuelAP/WhatsApp-UI",
-    description: "Clon de la interfaz principal de WhatsApp Android 📱.",
-  },
+	// {
+	//   name: "Portafolio",
+	//   imgURL: "images/miniatures/miniature-proyect-1.jpg",
+	//   tags: ["Materialize", "JS"],
+	//   liveURL: "https://jsamuelap.github.io",
+	//   codeURL: "https://github.com/JSamuelAP/JSamuelAP.github.io",
+	//   description:
+	//     "Mi portafolio de desarrollador para mostrar mis habilidades 📁.",
+	// },
+	// {
+	//   name: "Goza México",
+	//   imgURL: "images/miniatures/miniature-proyect-5.jpg",
+	//   tags: ["Sass", "JS", "Animaciones"],
+	//   liveURL: "https://jsamuelap.github.io/goza-mexico",
+	//   codeURL: "https://github.com/JSamuelAP/goza-mexico",
+	//   description:
+	//     "Sitio web de galerías de los lugares más impresionantes de México 🇲🇽.",
+	// },
+	// {
+	//   name: "Club Astucia",
+	//   imgURL: "images/miniatures/miniature-proyect-6.jpg",
+	//   tags: ["Responsive", "Flexbox", "Grid"],
+	//   liveURL: "https://jsamuelap.github.io/AstuciaWeb",
+	//   codeURL: "https://github.com/JSamuelAP/AstuciaWeb",
+	//   description: "Página web sobre el club de fútbol ficticio Astucia ⚽.",
+	// },
+	// {
+	//     name: "Fylo",
+	//     imgURL: "images/miniatures/miniature-proyect-2.jpg",
+	//     tags: ["Bootstrap", "Sass"],
+	//     liveURL: "https://jsamuelap.github.io/fylo-landing-page",
+	//     codeURL: "https://github.com/JSamuelAP/fylo-landing-page",
+	//     description:
+	//       "Landing Page, este proyecto es un reto 🎯 tomado de Frontend Mentor.",
+	//   },
+	{
+		name: "SM Dashboard",
+		imgURL: "images/miniatures/miniature-proyect-12.jpg",
+		tags: ["React", "Sass"],
+		liveURL: "https://jsamuelap.github.io/social-media-dashboard",
+		codeURL: "https://github.com/JSamuelAP/social-media-dashboard",
+		description:
+			"Panel de redes sociales con tema claro y oscuro, este proyecto es un reto tomado de Frontend Mentor 🎯",
+	},
+	{
+		name: "Catar 2022",
+		imgURL: "images/miniatures/miniature-proyect-6.jpg",
+		tags: ["JS", "Bootstrap"],
+		liveURL: "https://jsamuelap.github.io/sorteo-catar2022",
+		codeURL: "https://github.com/JSamuelAP/sorteo-catar2022",
+		description: "Simulador del sorteo de grupos del mundial de Catar 🇶🇦🏆⚽",
+	},
+	{
+		name: "Modo oscuro",
+		imgURL: "images/miniatures/miniature-proyect-5.jpg",
+		tags: ["JS", "Materialize"],
+		liveURL: "https://jsamuelap.github.io/dark-mode",
+		codeURL: "https://github.com/JSamuelAP/dark-mode",
+		description:
+			"Aplicación web que permite cambiar entre modos claro y oscuro 🌞🌙",
+	},
+	{
+		name: "API Clima",
+		imgURL: "images/miniatures/miniature-proyect-4.jpg",
+		tags: ["JS", "Fetch API"],
+		liveURL: "https://jsamuelap.github.io/Clima",
+		codeURL: "https://github.com/JSamuelAP/Clima",
+		description:
+			"Consulta el clima de una ciudad en tiempo real gracias a una API ⛅",
+	},
+	{
+		name: "CRUD JS",
+		imgURL: "images/miniatures/miniature-proyect-3.jpg",
+		tags: ["JS", "Bootstrap"],
+		liveURL: "https://jsamuelap.github.io/administrador-asignaturas",
+		codeURL: "https://github.com/JSamuelAP/administrador-asignaturas",
+		description: "Aplicación web para administrar materias escolares 📚",
+	},
+
+	{
+		name: "WhatsApp UI",
+		imgURL: "images/miniatures/miniature-proyect-1.jpg",
+		tags: ["Materialize", "CSS"],
+		liveURL: "https://jsamuelap.github.io/WhatsApp-UI",
+		codeURL: "https://github.com/JSamuelAP/WhatsApp-UI",
+		description: "Clon de la interfaz principal de WhatsApp Android 📱",
+	},
 ];
