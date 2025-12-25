208f399403c048e84603b2ae2f14580f5d4558f9
JSamuelAP <sp46191689@gmail.com>
2025-12-25 17:44:01 -0600

    feat: crear sección de habilidades
    - Se agregan imagenes svg de los iconos de las tecnologías y
      habilidades.
    - En los items de los menu se cambia stack por skills.
    - Se crea archivo de utilidades.

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,12 +1,12 @@
-export interface NavItem {
+export type NavItem = {
   label: string;
   icon: string;
   href: string;
-}
+};
 
 export const navigationItems: NavItem[] = [
   { label: 'Inicio', icon: 'home', href: '/' },
-  { label: 'Stack', icon: 'tools', href: '/#stack' },
+  { label: 'Habilidades', icon: 'tools', href: '/#skills' },
   { label: 'Experiencia', icon: 'briefcase', href: '/#experience' },
   { label: 'Proyectos', icon: 'app-window', href: '/#projects' },
   { label: 'Escolaridad', icon: 'school', href: '/#education' },
