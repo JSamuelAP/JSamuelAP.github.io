7e87536f7dff011ed3f279a971d339488be80b58
JSamuelAP <sp46191689@gmail.com>
2025-12-28 15:25:17 -0600

    feat: crear seccion de proyectos

---
+++ src/config/projects.ts
@@ -0,0 +1,45 @@
+import thumbnailPlaceholder from '../assets/images/projects/thumbnail-placeholder.png';
+import capysoftThumbnail from '../assets/images/projects/capysoft-thumbnail.jpeg';
+import innovaTubeThumbnail from '../assets/images/projects/innovatube-thumbnail.jpeg';
+
+export type Project = {
+  id: string;
+  name: string;
+  description: string;
+  thumbnail: ImageMetadata;
+  technologies: string[];
+  repositoryUrl?: string;
+  liveDemoUrl?: string;
+};
+
+export const projects: Project[] = [
+  {
+    id: 'portfolio',
+    name: 'Mi portafolio web',
+    description:
+      'Sitio web estático donde muestro mis habilidades y proyectos para darme a conocer como desarrollador de software.',
+    thumbnail: thumbnailPlaceholder,
+    technologies: ['Astro', 'Tailwind CSS', 'DaisyUI'],
+    repositoryUrl: 'https://github.com/JSamuelAP/JSamuelAP.github.io',
+    liveDemoUrl: 'https://jsamuelap.github.io',
+  },
+  {
+    id: 'innovatube',
+    name: 'InnovaTube',
+    description:
+      'Plataforma para buscar videos y guardarlos como favoritos. Fue una prueba técnica del área de innovación en Grupo Castores.',
+    thumbnail: innovaTubeThumbnail,
+    technologies: ['Angular', 'PrimeNG', 'Node.js', 'TypeScript', 'Express.js', 'Supabase'],
+    repositoryUrl: 'https://github.com/JSamuelAP/innovatube',
+    liveDemoUrl: 'https://innovatube-jsamuelap.netlify.app',
+  },
+  {
+    id: 'capysoft',
+    name: 'CapySoft',
+    description:
+      'Punto de venta para la cafeteria El rincón del Capibara. Proyecto escolar que incluye microservicios.',
+    thumbnail: capysoftThumbnail,
+    technologies: ['Angular', 'PrimeNG', 'Java', 'Spring Boot', 'MySQL', 'Docker'],
+    repositoryUrl: 'https://github.com/JSamuelAP/capysoft',
+  },
+];
